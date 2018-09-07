ff8f21209a9a61415c59c85218d9c6028bcd363c
Matt Moore <mattmoore@carbonhelix.com>
2018-09-07 14:51:01 -0500

    io-monad-scaled-benchmark

--- io-monad-scaled-benchmark/app/Main.hs
+++ io-monad-scaled-benchmark/app/Main.hs
@@ -12,10 +12,19 @@ files :: Int -> [String]
 files count = map (\x -> "files/data" ++ show x ++ ".txt") [1..count]
 
 main :: IO ()
-main = do
-  args <- getArgs
-  start <- getTime Monotonic
-  results <- fetchFiles ((files . read . head) args)
-  end <- getTime Monotonic
-  putStrLn (merge results)
-  fprint (timeSpecs % "\n") start end
+-- main = do
+--   args <- getArgs
+--   let fileList = (files . read . head) args
+--   start <- getTime Monotonic
+--   results <- fetchFiles fileList
+--   end <- getTime Monotonic
+--   putStrLn (merge results)
+--   fprint (timeSpecs % "\n") start end
+
+main =
+  fmap (files . read . head) getArgs >>= \fileList ->
+  getTime Monotonic >>= \start ->
+  fetchFiles fileList >>= \contents ->
+  getTime Monotonic >>= \end ->
+  (putStrLn . merge) contents
+  >> fprint (timeSpecs % "\n") start end
