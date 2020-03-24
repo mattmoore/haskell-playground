b8c7a14a63a250ad878aa6d0af8ad4c61b2f066d
Matt Moore <matt@mattmoore.io>
2020-03-23 22:28:32 -0500

    Add functor and applicative examples with fmap and <$>/<*>.

--- monads/test/MaybeSpec.hs
+++ monads/test/MaybeSpec.hs
@@ -13,5 +13,19 @@ spec = do
     it "has a Nothing type that contains no value and replaces 'null' found in many languages" $ do
       (Nothing :: Maybe ()) `shouldBe` (Nothing :: Maybe ())
 
+  describe "Just is a functor" $ do
+
     it "can be mapped over" $ do
       fmap (+1) (Just 1) `shouldBe` Just 2
+
+    it "fmap has an infix version <$>" $ do
+      (+1) <$> (Just 1) `shouldBe` Just 2
+
+    it "<$> can be applied to functors. we call these applicatives" $ do
+      (+) <$> (Just 1) <*> (Just 1) `shouldBe` Just 2
+
+    it "fmap can be applied to lists as well, since they're functors too" $ do
+      fmap (*2) [1, 2, 3] `shouldBe` [2, 4, 6]
+
+    it "same thing can be done with <$>" $ do
+      (*2) <$> [1, 2, 3] `shouldBe` [2, 4, 6]
