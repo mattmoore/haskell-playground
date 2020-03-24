75fad324b09809c06c422863c1f5d8f8f40a252f
Matt Moore <matt@mattmoore.io>
2020-03-23 22:12:12 -0500

    Add Nothing example.

--- monads/test/MaybeSpec.hs
+++ monads/test/MaybeSpec.hs
@@ -5,7 +5,10 @@ import Test.Hspec
 spec :: Spec
 spec = do
 
-  describe "Maybe" $ do
+  describe "Maybe is an algebraic data type (ADT) made of Just and Nothing" $ do
 
-    it "is an algebraic data type (ADT) made of Just and Nothing" $ do
+    it "has a Just type that contains a value" $ do
       Just 1 `shouldBe` Just 1
+
+    it "has a Nothing type that contains no value and replaces 'null' found in many languages" $ do
+      (Nothing :: Maybe ()) `shouldBe` (Nothing :: Maybe ())
