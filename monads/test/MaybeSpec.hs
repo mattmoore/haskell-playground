40dc526cc38121b67c2a29d35a3bba3117a3a862
Matt Moore <matt@mattmoore.io>
2021-11-20 18:57:41 -0600

    Update MaybeSpec

--- monads/test/MaybeSpec.hs
+++ monads/test/MaybeSpec.hs
@@ -6,15 +6,17 @@ spec :: Spec
 spec = do
 
   describe "Maybe is an algebraic data type (ADT) made of Just and Nothing" $ do
-
     it "has a Just type that contains a value" $ do
       Just 1 `shouldBe` Just 1
 
     it "has a Nothing type that contains no value and replaces 'null' found in many languages" $ do
       (Nothing :: Maybe ()) `shouldBe` (Nothing :: Maybe ())
 
-  describe "Just is a functor" $ do
+  describe "Maybe takes something and if it's a value, it returns (Just value), else Nothing" $ do
+    it "should be Just 1" $ do
+      (pure 1) `shouldBe` Just 1
 
+  describe "Just is a functor" $ do
     it "can be mapped over" $ do
       fmap (+1) (Just 1) `shouldBe` Just 2
 
